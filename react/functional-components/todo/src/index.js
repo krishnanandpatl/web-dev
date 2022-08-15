6c4cea80dc09fad7c25036c8493a6d2150c462a6
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-08-15 12:23:09 +0530

    frontend changes

--- react/functional-components/todo/src/index.js
+++ react/functional-components/todo/src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React,{useState} from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Addtask(){
+  return <form>
+        <input type="text" value=""></input>
+        <input type="button" value="Add Task"></input>
+    </form>
+}
+
+function Tasklist(props){
+
+  return <div className={props.purpose}>
+  <div className="list-container">
+  <div className="title">{props.task}</div>
+  <div className='content'>
+      
+  </div>
+  </div>
+</div>
+}
+
+function App(){
+
+  return <div>
+  <div className='add-task'><Addtask></Addtask></div>
+  <div className='task-lists'>
+    <Tasklist purpose="todo" task="Todo"></Tasklist>
+    <Tasklist purpose="finished" task="Finished"></Tasklist>
+  </div>
+  </div>
+
+}
+
+ReactDOM.render(
+  <App></App>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
