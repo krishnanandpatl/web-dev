5177577ab9e18cc3af17910e3a47a54f56cadca4
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-08-27 14:53:37 +0530

    UseEffect POC

--- react/functional-components/reels/src/App.js
+++ react/functional-components/reels/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
+
 
 function App() {
+  let [list,setList]=useState([]);
+ const handleDelete=function(idx){
+  let dd=[...list];
+  dd.splice(idx,1);
+  setList(dd);
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>Todo Example</div>
+    <InputBox setList={setList} list={list}/>
+    <TaskList tasks={list} handleDelete={handleDelete}/>
+    </>
   );
+}
+function InputBox(props){
+  let [task,setTask]=useState("");
+  const handleChange=function(e){
+      setTask(e.target.value);
+  }
+  const handleTask=function(){
+    let newlist=[...props.list];
+    newlist.push(task);
+    props.setList(newlist);
+  }
+   return(
+    <>
+      <input type="text" onChange={handleChange}></input>
+      <button onClick={handleTask}>Add task</button>
+    </>
+   );
+}
+function TaskList(props){
+  let {tasks,handleDelete}=props;
+  
+  return(
+    <div>
+      {
+        tasks.map(function(task,idx){
+          return (
+            <>
+          <li key={idx}>{task}</li>
+          <button onClick={()=>{handleDelete(idx)}}>Delete</button>
+          </>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
