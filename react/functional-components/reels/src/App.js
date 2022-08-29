0c7b98d337258a559bed84df684fb5ecc1a88c7b
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-08-29 20:11:22 +0530

    Reels Route built

--- react/functional-components/reels/src/App.js
+++ react/functional-components/reels/src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
 import React from 'react'
-import ThemeChange from './Theme Change/ThemeChange';
-
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Feed from './components/Feed';
+import Forget from './components/Forget';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
  
   return (
-    <>
-    <ThemeChange/>
-    </>
+    <Switch>
+      <Route path='/login' exact>
+        <Login/>
+      </Route>
+      <Route path='/signup' exact>
+        <Signup/>
+      </Route>
+      <Route path='/forget' exact>
+        <Forget/>
+      </Route>
+      <Route path='/profile' exact>
+        <Profile/>
+      </Route>
+      <Route path='/' exact>
+        <Feed/>
+      </Route>
+      <Route>
+        <PageNotFound/>
+      </Route>
+    </Switch>
   );
 }
 
