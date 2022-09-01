ff2c01522502a7df33f40ffc07b67ca69a5d85eb
Krishna Nand Patel <imkrishna8737@gmail.com>
2022-09-01 16:35:25 +0530

    profile designing done

--- react/functional-components/reels/src/components/Profile.jsx
+++ react/functional-components/reels/src/components/Profile.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './profile.css';
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+    <div className='header'></div>
+    <div className="main">
+      <div className="pimg_container">
+         <img src="http://via.placeholder.com/640x360" alt="Profile" className="pimg" />
+
+      </div>
+      <div className="details">
+        <div className="content">Krishna</div>
+        <div className='content'>No. of Post: <span className="bold_text">Posts</span></div>
+        <div className="content">Email <span className="bold_text">@ddd.com</span></div>
+      </div>
+    </div>
+    </>
   )
 }
 
